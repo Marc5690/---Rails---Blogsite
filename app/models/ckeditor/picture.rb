6d8fce84d7ae2fae89959d7fc8a44266757719a2
marclaffan@gmail.com <marclaffan@gmail.com>
2017-03-22 18:43:34 +0000

    Image fix

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,14 +1,22 @@
 class Ckeditor::Picture < Ckeditor::Asset
+ 
+
   has_attached_file :data,
                     url: '/ckeditor_assets/pictures/:id/:style_:basename.:extension',
                     path: ':rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension',
                     styles: { content: '800>', thumb: '118x100#' }
-
-  validates_attachment_presence :data
-  validates_attachment_size :data, less_than: 2.megabytes
-  #validates_attachment_content_type :data, content_type: /\Aimage/
-
+  
   def url_content
     url(:content)
   end
+
+  validates_attachment_presence :data
+  validates_attachment_size :data, less_than: 100.megabytes
+  validates_attachment_content_type :data, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif", "application/pdf"]
+  #do_not_validate_attachment_file_type :
+  #validates_attachment_content_type :data, content_type: /\Aimage/
+  #validates_attachment_content_type :image,:content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
+  #validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,5 @@ RAILS_ENV="production"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+#Paperclip::Attachment.default_options[:validate_media_type] = false
+
